ba91def727bd14f83d94864906f47b4c250d1a75
ernstharnie <ernst_harnie@hotmail.com>
2016-04-12 18:49:31 +0200

    code opgeruimd

--- chuck_norris/urls.py
+++ chuck_norris/urls.py
@@ -5,6 +5,4 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^joke$', views.joke, name='joke')
-	#url(r'^$', views.joke, name='joke'),
-    #url(r'^(?P<character_id>[0-9]+)/$', views.character, name='character'),
 ]
