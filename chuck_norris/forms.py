60531e65f4dd25c707f05f33583c16ca17089a0f
ernstharnie <ernst_harnie@hotmail.com>
2016-04-12 18:47:38 +0200

    view werkt

--- chuck_norris/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
- 
-class PostForm(forms.Form):
-	voornaam = forms.CharField(max_length=256)
-	achternaam = forms.CharField(max_length=256)
-	fields = ['voornaam', 'achtenraam']
--- chuck_norris/views.py
+++ chuck_norris/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from chuck_norris.forms import PostForm
 import urllib2 as ur
 from urllib2 import urlopen
 import json
-
 import redis
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
-# Create your views here.
+
 
 def index(request):
 	return render(request, 'chuck_norris/index.html', {})
@@ -16,17 +14,16 @@ def index(request):
 def joke(request):
 	joke = ''
 	form = request.user
-	voornaam = request.POST.get("voornaam", "")
-	achternaam = request.POST.get("achternaam", "")
-	naam = voornaam = achternaam
-	url = ''
+	voornaam = request.POST['voornaam']
+	achternaam = request.POST['achternaam']
+	naam = voornaam + achternaam
+
 	if not r.exists(naam):
-		urlString = str('http://api.icndb.com/jokes/random?firstName=John' + voornaam + '&lastName=' + achternaam)					# create url
+		urlString = str('http://api.icndb.com/jokes/random?firstName=' + voornaam + '&lastName=' + achternaam)					# create url
 		response = ur.urlopen(urlString).read() 
-		result = json.loads(response.decode('utf-8'))		
-    									# else
+		joke = json.loads(response.decode('utf-8'))		
 		r.set(naam, json.dumps(joke))			
 	else:													
 		response = r.get(naam)						
 		joke = json.loads(response.decode('utf-8'))		
-	return render(request, 'chuck_norris/joke.html', {'joke': joke})
+	return render(request, 'chuck_norris/joke.html', {'joke': joke['value']['joke']})
